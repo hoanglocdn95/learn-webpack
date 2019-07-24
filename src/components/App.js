924c18aef76de9d19b17333a9b2b085363031aeb
hoanglocdn95 <tran.hoang.loc@framgia.com>
2019-07-24 10:08:14 +0700

    learn webpack

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='red'>That's WEBFUCK with Webpack</div>
+    );
+  }
+}
+
+export default App;
